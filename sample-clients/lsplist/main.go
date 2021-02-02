350db7ef04e77c6fb4de6c14cdebb7c6dbbf4651
Yaacov Akiba Slama <yaslama@breez.technology>
2021-02-02 16:14:06 +0200

    Fix lsplist sample client

--- sample-clients/lsplist/main.go
+++ sample-clients/lsplist/main.go
@@ -8,8 +8,12 @@ import (
 
 	"github.com/breez/server/breez"
 	. "github.com/breez/server/sample-clients"
-
 	"github.com/joho/godotenv"
+	"google.golang.org/grpc/metadata"
+)
+
+const (
+	lspToken = "1WL4gWZLZJ40qkXpeiUzJE7GCo4WhRXZXJbdhuP7GLg="
 )
 
 func main() {
@@ -24,8 +28,10 @@ func main() {
 	}
 	defer conn.Close()
 	c := breez.NewChannelOpenerClient(conn)
-
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	ctx, cancel := context.WithTimeout(
+		metadata.AppendToOutgoingContext(context.Background(), "authorization", "Bearer "+lspToken),
+		1*time.Second,
+	)
 	defer cancel()
 	r, err := c.LSPList(ctx, &breez.LSPListRequest{Pubkey: "testpubkey"})
 	if err != nil {
