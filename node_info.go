b4309528df3b354864d4b7ebde35af5a3df9710b
Roei Erez <roeierez@gmail.com>
2021-11-28 18:19:49 +0200

    change only keys to use string type

--- node_info.go
+++ node_info.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"bytes"
 	"context"
 	"encoding/hex"
 	"errors"
@@ -12,7 +11,7 @@ import (
 	"time"
 
 	"github.com/breez/server/breez"
-	"github.com/lightningnetwork/lnd/lnrpc"
+	"github.com/lightningnetwork/lnd/lnrpc/signrpc"
 	"google.golang.org/grpc/metadata"
 )
 
@@ -29,40 +28,37 @@ var (
 func (s *server) SetNodeInfo(ctx context.Context, in *breez.SetNodeInfoRequest) (*breez.SetNodeInfoResponse, error) {
 	clientCtx := metadata.AppendToOutgoingContext(context.Background(), "macaroon", os.Getenv("LND_MACAROON_HEX"))
 
-	if _, ok := allowedKeys[string(in.Key)]; !ok {
+	if _, ok := allowedKeys[in.Key]; !ok {
 		return nil, ErrKeyNotSupported
 	}
-	requestTimeDiff := time.Now().Sub(time.Unix(in.Timestamp, 0))
+	requestTimeDiff := time.Since(time.Unix(in.Timestamp, 0))
 	if math.Abs(float64(requestTimeDiff)) > float64(maxRequesTimeDiff) {
 		return nil, ErrInvalidTimestamp
 	}
 
 	// concatenate all request payload fields
-	msg := fmt.Sprintf("%v-%v-%v", hex.EncodeToString(in.Key), hex.EncodeToString(in.Value), in.Timestamp)
+	msg := fmt.Sprintf("%v-%v-%v", in.Key, hex.EncodeToString(in.Value), in.Timestamp)
 
 	// Verify the message
-	verificationResponse, err := client.VerifyMessage(clientCtx, &lnrpc.VerifyMessageRequest{
+	verificationResponse, err := signerClient.VerifyMessage(clientCtx, &signrpc.VerifyMessageReq{
 		Msg:       []byte(msg),
+		Pubkey:    in.Pubkey,
 		Signature: in.Signature,
 	})
 	if err != nil {
 		return nil, err
 	}
-	pubkeyBytes, err := hex.DecodeString(verificationResponse.Pubkey)
-	if err != nil {
-		return nil, err
-	}
 
-	if !verificationResponse.Valid || !bytes.Equal(pubkeyBytes, in.Pubkey) {
+	if !verificationResponse.Valid {
 		return nil, errors.New("failed to verify value")
 	}
 
 	// Update the value in redis and set expiration of 1 hour.
-	redisKey := fmt.Sprintf("%v-%v", string(in.Pubkey), string(in.Key))
+	redisKey := fmt.Sprintf("%v-%v", hex.EncodeToString(in.Pubkey), in.Key)
 	if err := updateKeyFields(redisKey, map[string]string{
-		"value":     string(in.Value),
+		"value":     hex.EncodeToString(in.Value),
 		"timestamp": strconv.FormatInt(in.Timestamp, 10),
-		"signature": in.Signature,
+		"signature": hex.EncodeToString(in.Signature),
 	}); err != nil {
 		return nil, fmt.Errorf("failed to update value")
 	}
@@ -75,37 +71,46 @@ func (s *server) SetNodeInfo(ctx context.Context, in *breez.SetNodeInfoRequest) 
 // GetNodeInfo is used by other participants to get the meeting information. They should verify the message using th
 // provided signature.
 func (s *server) GetNodeInfo(ctx context.Context, in *breez.GetNodeInfoRequest) (*breez.GetNodeInfoResponse, error) {
-	keyStr := string(in.Key)
-	if _, ok := allowedKeys[keyStr]; !ok {
+	if _, ok := allowedKeys[in.Key]; !ok {
 		return nil, ErrKeyNotSupported
 	}
 
-	redisKey := fmt.Sprintf("%v-%v", string(in.Pubkey), string(in.Key))
+	redisKey := fmt.Sprintf("%v-%v", hex.EncodeToString(in.Pubkey), in.Key)
 	fields, err := getKeyFields(redisKey)
 	if err != nil {
 		return nil, err
 	}
 
-	rawVal, ok := fields["value"]
+	value, ok := fields["value"]
 	if !ok {
 		return nil, fmt.Errorf("failed to get value")
 	}
+	valueBytes, err := hex.DecodeString(value)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get value")
+	}
+
 	signature, ok := fields["signature"]
 	if !ok {
 		return nil, fmt.Errorf("failed to get value")
 	}
+	signatureBytes, err := hex.DecodeString(signature)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get value")
+	}
+
 	timestampStr, ok := fields["timestamp"]
 	if !ok {
 		return nil, fmt.Errorf("failed to get value")
 	}
-
 	timestamp, err := strconv.ParseInt(timestampStr, 10, 64)
 	if err != nil {
 		return nil, fmt.Errorf("failed to get value")
 	}
+
 	return &breez.GetNodeInfoResponse{
-		Value:     []byte(rawVal),
+		Value:     valueBytes,
 		Timestamp: timestamp,
-		Signature: signature,
+		Signature: signatureBytes,
 	}, nil
 }
