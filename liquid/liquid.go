87f8fa8e439e93e538cdadc58908f89ac5ef1981
Yaacov Akiba Slama <yaslama@breez.technology>
2024-09-08 16:30:06 +0300

    The api key is a certificate signed by our CA

--- liquid/liquid.go
+++ liquid/liquid.go
@@ -3,13 +3,18 @@ package liquid
 import (
 	"bytes"
 	"crypto/sha256"
+	"crypto/x509"
+	"encoding/base64"
 	"encoding/json"
+	"encoding/pem"
 	"errors"
 	"io"
 	"log"
 	"net/http"
 	"net/http/httputil"
 	"net/url"
+	"os"
+	"strings"
 	"time"
 
 	"github.com/VictoriaMetrics/fastcache"
@@ -68,26 +73,72 @@ func BroadcastHandler(chainApiServers []*breez.ChainApiServersReply_ChainAPIServ
 		log.Fatal("Liquid: No api URL found")
 	}
 	fc := fastcache.New(100_000_000)
+
+	CACertBlock, _ := pem.Decode([]byte(os.Getenv("BREEZ_CA_CERT")))
+	if CACertBlock == nil {
+		log.Fatal("Breez CA cert invalid")
+	}
+	CACert, err := x509.ParseCertificate(CACertBlock.Bytes)
+	if err != nil {
+		log.Fatal("Cannot parse CA cert:", err)
+	}
+
+	rootPool := x509.NewCertPool()
+	rootPool.AddCert(CACert)
+
 	return func(w http.ResponseWriter, r *http.Request) {
-		swapID := r.Header.Get("Swap-Id")
-		if swapID == "" {
-			log.Printf("Liquid: No swapID")
-			w.WriteHeader(http.StatusBadRequest)
-			return
+
+		authHeader := r.Header.Get("authorization")
+		if len(authHeader) > 7 && strings.HasPrefix(authHeader, "Bearer ") {
+			apiKey := authHeader[7:]
+			block, err := base64.StdEncoding.DecodeString(apiKey)
+			if err != nil {
+				log.Printf("base64.StdEncoding.DecodeString(apiKey) [%v] error: %v", apiKey, err)
+				w.WriteHeader(http.StatusBadRequest)
+				return
+			}
+			cert, err := x509.ParseCertificate(block)
+			if err != nil {
+				log.Printf("Cannot parse cert: %v", err)
+				w.WriteHeader(http.StatusBadRequest)
+				return
+			}
+			chains, err := cert.Verify(x509.VerifyOptions{
+				Roots: rootPool,
+			})
+			if err != nil {
+				log.Printf("cert.Verify error: %v", err)
+				w.WriteHeader(http.StatusBadRequest)
+				return
+			}
+			if len(chains) != 1 || len(chains[0]) != 2 || !chains[0][0].Equal(cert) || !chains[0][1].Equal(CACert) {
+				log.Printf("cert verification error")
+				w.WriteHeader(http.StatusBadRequest)
+				return
+			}
+		} else {
+			swapID := r.Header.Get("Swap-Id")
+			if swapID == "" {
+				log.Printf("Liquid: No swapID")
+				w.WriteHeader(http.StatusBadRequest)
+				return
+			}
+			body, err := io.ReadAll(io.LimitReader(r.Body, 100_000))
+			if err != nil {
+				log.Printf("Liquid: error reading body: %v", err)
+				w.WriteHeader(http.StatusBadRequest)
+				return
+			}
+			err = checkSwapId(fc, apiURL, swapID, body)
+			if err != nil {
+				log.Printf("Liquid: error when checking swapID=%v : %v", swapID, err)
+				w.WriteHeader(http.StatusBadRequest)
+				return
+			}
+
+			r.Body = io.NopCloser(bytes.NewBuffer(body))
 		}
-		body, err := io.ReadAll(io.LimitReader(r.Body, 100_000))
-		if err != nil {
-			log.Printf("Liquid: error reading body: %v", err)
-			w.WriteHeader(http.StatusBadRequest)
-			return
-		}
-		err = checkSwapId(fc, apiURL, swapID, body)
-		if err != nil {
-			log.Printf("Liquid: error when checking swapID=%v : %v", swapID, err)
-			w.WriteHeader(http.StatusBadRequest)
-			return
-		}
-		r.Body = io.NopCloser(bytes.NewBuffer(body))
+
 		r.Host = u.Host
 		http.StripPrefix(prefix, p).ServeHTTP(w, r)
 	}
