6eb842345848c013d0d84859576f12baed156596
Yaacov Akiba Slama <yaslama@breez.technology>
2025-02-24 16:21:53 +0200

    Update lsplist sample

--- sample-clients/lsplist/main.go
+++ sample-clients/lsplist/main.go
@@ -33,15 +33,51 @@ func main() {
 		1*time.Second,
 	)
 	defer cancel()
+	lspList(c, ctx)
+	fmt.Println(" ---- ---- ----")
+	lspFullList(c, ctx)
+}
+
+func lspList(c breez.ChannelOpenerClient, ctx context.Context) {
 	r, err := c.LSPList(ctx, &breez.LSPListRequest{Pubkey: "testpubkey"})
 	if err != nil {
 		log.Fatalf("err: %v", err)
 	}
 	for id, info := range r.Lsps {
 		fmt.Printf("%s: %#v\n", id, info)
-		fmt.Println("OpeningFeeParamsMenu")
-		for _, m := range info.OpeningFeeParamsMenu {
-			fmt.Printf("%#v\n", m)
+		if info.OpeningFeeParamsMenu == nil {
+			fmt.Println("NO OpeningFeeParamsMenu - PROBLEM")
+		} else {
+			fmt.Println("OpeningFeeParamsMenu")
+			for _, m := range info.OpeningFeeParamsMenu {
+				fmt.Printf("%#v\n", m)
+				fmt.Printf("m.MinMsat: %v\n", m.MinMsat)
+				fmt.Printf("m.Proportional: %v\n", m.Proportional)
+				fmt.Printf("m.ValidUntil: %v\n", m.ValidUntil)
+			}
 		}
+		fmt.Println()
+	}
+}
+
+func lspFullList(c breez.ChannelOpenerClient, ctx context.Context) {
+	r, err := c.LSPFullList(ctx, &breez.LSPFullListRequest{Pubkey: "testpubkey"})
+	if err != nil {
+		log.Fatalf("err: %v", err)
+	}
+	for i, info := range r.Lsps {
+		fmt.Printf("%s: %#v\n", i, info)
+		if info.OpeningFeeParamsMenu == nil {
+			fmt.Println("NO OpeningFeeParamsMenu - PROBLEM - inactive")
+		} else {
+			fmt.Println("OpeningFeeParamsMenu")
+			for _, m := range info.OpeningFeeParamsMenu {
+				fmt.Printf("%#v\n", m)
+				fmt.Printf("m.MinMsat: %v\n", m.MinMsat)
+				fmt.Printf("m.Proportional: %v\n", m.Proportional)
+				fmt.Printf("m.ValidUntil: %v\n", m.ValidUntil)
+			}
+		}
+		fmt.Println()
 	}
 }
