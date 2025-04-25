45067dce03eb5bb8b7aa83df3defcf5c9371e868
Jesse de Wit <witdejesse@hotmail.com>
2025-04-25 10:10:41 +0200

    remove taproot swaps api key restriction

--- swapd/server.go
+++ swapd/server.go
@@ -2,17 +2,9 @@ package swapd
 
 import (
 	"context"
-	"errors"
-	"os"
-	"strings"
 
-	"slices"
-
-	"github.com/breez/server/auth"
 	"github.com/breez/server/breez"
 )
-
-var errNoTaprootKey = errors.New("not a taproot swap api key")
 
 type swapdServer struct {
 	client breez.TaprootSwapperClient
@@ -26,50 +18,17 @@ func NewServer(client breez.TaprootSwapperClient) breez.TaprootSwapperServer {
 }
 
 func (s *swapdServer) CreateSwap(ctx context.Context, in *breez.CreateSwapRequest) (*breez.CreateSwapResponse, error) {
-	err := ensureTaprootSwapKey(ctx)
-	if err != nil {
-		return nil, err
-	}
 	return s.client.CreateSwap(ctx, in)
 }
 
 func (s *swapdServer) PaySwap(ctx context.Context, in *breez.PaySwapRequest) (*breez.PaySwapResponse, error) {
-	err := ensureTaprootSwapKey(ctx)
-	if err != nil {
-		return nil, err
-	}
 	return s.client.PaySwap(ctx, in)
 }
 
 func (s *swapdServer) RefundSwap(ctx context.Context, in *breez.RefundSwapRequest) (*breez.RefundSwapResponse, error) {
-	err := ensureTaprootSwapKey(ctx)
-	if err != nil {
-		return nil, err
-	}
 	return s.client.RefundSwap(ctx, in)
 }
 
 func (s *swapdServer) SwapParameters(ctx context.Context, in *breez.SwapParametersRequest) (*breez.SwapParametersResponse, error) {
-	err := ensureTaprootSwapKey(ctx)
-	if err != nil {
-		return nil, err
-	}
 	return s.client.SwapParameters(ctx, in)
-}
-
-func ensureTaprootSwapKey(ctx context.Context) error {
-	keysStr := os.Getenv("TAPROOT_API_KEYS")
-	if keysStr == "" {
-		return errNoTaprootKey
-	}
-
-	validKeys := strings.Split(keysStr, ",")
-	clientKeys := auth.GetHeaderKeys(ctx)
-	for _, key := range clientKeys {
-		if slices.Contains(validKeys, key) {
-			return nil
-		}
-	}
-
-	return errNoTaprootKey
 }
